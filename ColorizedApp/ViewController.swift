3d9b622c96a8eba79edb28aeea16aed18ac72420
Manohin <111537419+Manohin@users.noreply.github.com>
2023-02-06 04:20:12 +0300

    Done!

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -8,12 +8,62 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
+    
+    @IBOutlet var redColorSlider: UISlider!
+    @IBOutlet var greenColorSlider: UISlider!
+    @IBOutlet var blueColorSlider: UISlider!
+    
+    @IBOutlet var colorDisplayView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        colorDisplayView.layer.cornerRadius = 15
+        
+        setLabelText(redValueLabel, redColorSlider)
+        setLabelText(greenValueLabel, greenColorSlider)
+        setLabelText(blueValueLabel, blueColorSlider)
+        
+        setColor()
     }
-
-
+    
+    @IBAction func redColorSliderAction() {
+        
+        setLabelText(redValueLabel, redColorSlider)
+        setColor()
+    }
+    
+    @IBAction func greenColorSliderAction() {
+        
+        setLabelText(greenValueLabel, greenColorSlider)
+        setColor()
+    }
+    
+    @IBAction func blueColorSliderAction() {
+        
+        setLabelText(blueValueLabel, blueColorSlider)
+        setColor()
+    }
+    
+    private func setLabelText(_ label: UILabel, _ slider: UISlider) {
+        
+        label.text = slider.value.formatted(
+            .number.scale(1.0)
+            .notation(.compactName)
+            .grouping(.automatic)
+        )
+    }
+    
+    private func setColor() {
+        
+        colorDisplayView.backgroundColor = UIColor(
+            red: CGFloat(redColorSlider.value),
+            green: CGFloat(greenColorSlider.value),
+            blue: CGFloat(blueColorSlider.value), alpha: 1)
+    }
 }
 
